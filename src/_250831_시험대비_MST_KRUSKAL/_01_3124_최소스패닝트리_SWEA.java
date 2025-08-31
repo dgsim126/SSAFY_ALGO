c42e6ff104f2d9e7874bc8b96f150d3b7c1cc223
donggeun <dgsim126@gmail.com>
2025-08-31 18:30:23 +0900

    algo

--- src/_250831_시험대비_MST_KRUSKAL/_01_3124_최소스패닝트리_SWEA.java
+++ src/_250831_시험대비_MST_KRUSKAL/_01_3124_최소스패닝트리_SWEA.java
@@ -1,9 +1,102 @@
 package _250831_시험대비_MST_KRUSKAL;
 
-public class _01_3124_최소스패닝트리_SWEA {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+/**
+ * 모든 노드를 연결하는 최소한의 간선을 구해야한다.
+ * 
+ * UNION-FIND를 응용하자.
+ * 일단 Queue에 (가중치, A번 정점, B번 정점)의 형태로 모든 값을 넣어놓는다.
+ * 가중치를 기준으로 정렬한다.
+ * 
+ * 해당 큐에서 값을 하나 뺀다.
+ * 빼낸 값 A, B를 find하여 같은 루트를 바라보는지 확인한다.
+ * 같은 루트를 바라보지 않는다면 해당 값을 연결시킨다.(연결시킨다는 의미는 더 큰 값의 parnet를 작은 값으로 설정하겠다는 의미)
+ * 
+ * 이렇게 사이클을 방지하며 크기가 작은 순서대로 연결하면 MST를 만들 수 있다.
+ */
+
+public class _01_3124_최소스패닝트리_SWEA {
+	
+	static int V; 
+	static int E;
+	static int parent[];
+	static PriorityQueue<long[]> queue;
+	static long result;
+	
+	static int find(int a) {
+		if(a==parent[a]) {
+			return a;
+		}
+		
+		return parent[a]= find(parent[a]);
+	}
+	
+	static void union(int a, int b) {
+		a= find(a);
+		b= find(b);
+		
+		if(a>b) {
+			parent[a]= b;
+		}else {
+			parent[b]= a;
+		}
+	}
+	
+	static void solution() {
+		result= 0;
+		
+		while(!queue.isEmpty()) {
+			long[] current= queue.poll();
+			int a= (int) current[0];
+			int b= (int) current[1];
+			long weight= current[2];
+			
+			if(find(a)==find(b)) { // 사이클 발생
+				continue;
+			}
+			
+			union(a, b);
+			result+=weight;
+		}
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T= Integer.parseInt(br.readLine());
+		
+		for(int tc=1; tc<T+1; tc++) {
+			st= new StringTokenizer(br.readLine());
+			V= Integer.parseInt(st.nextToken());
+			E= Integer.parseInt(st.nextToken());
+			
+			parent= new int[V+1];
+			queue= new PriorityQueue<>((a, b)->Long.compare(a[2], b[2]));
+			
+			for(int i=1; i<V+1; i++) {
+				parent[i]= i;
+			}
+			
+			for(int i=0; i<E; i++) {
+				st= new StringTokenizer(br.readLine());
+				
+				int temp1= Integer.parseInt(st.nextToken());
+				int temp2= Integer.parseInt(st.nextToken());
+				int temp3= Integer.parseInt(st.nextToken());
+				
+				queue.add(new long[] {temp1, temp2, temp3});
+			}
+			
+			solution();
+			
+			System.out.println("#"+tc+" "+result);
+		}
 
 	}
 
--- src/_250831_시험대비_위상정렬/_01_1267_작업순서_SWEA.java
+++ src/_250831_시험대비_위상정렬/_01_1267_작업순서_SWEA.java
@@ -1,10 +1,99 @@
 package _250831_시험대비_위상정렬;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/**
+ * 진입차수만 기억하면 된다.
+ * Node class를 만든다.
+ * 그리고 각각의 node는 connected[]를 갖는다(현재 노드에서 나가는 방향)
+ * 
+ * weight[]를 만든다. 특정 node로 화살표가 향한다면, 해당 index를 +1한다.
+ * 
+ * 1. weight를 돌며 weight[index]==0인 값들을 queue에 넣는다.
+ * 2. 큐에서 값을 하나 뺀다(이 시점에 index번호 출력)
+ * 3. 해당 노드와 연결된 connected를 찾아 weight를 감소시킨다. (해당 노드는 이미 뺏으니 weight를 -1처리)
+ * 4. 만약 더 이상 큐에 값이 없다면 다시 0을 찾는다.
+ */
+
 public class _01_1267_작업순서_SWEA {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static class Node{
+		List<Integer> connected= new ArrayList<>();
+	}
+	
+	static int V;
+	static int E;
+	static Node node[];
+	static int weight[]; // 진입차수 기록(나에게로 화살표가 들어오는 경우를 cnt한다.)
+	
+	static int toFindZero() {
+		for(int i=1; i<V+1; i++) {
+			if(weight[i]==0) {
+				weight[i]= -1;
+				return i;
+			}
+		}
+		
+		return -1;
+	}
+	
+	static void solution() {
+		Queue<Integer> queue= new LinkedList<>();
+		
+		for(int i=0; i<V; i++) { // 노드의 개수만큼
+			int temp= toFindZero();
+			
+			queue.add(temp);
+			
+			while(!queue.isEmpty()) {
+				int current= queue.poll();
+				System.out.print(current+" ");
+				
+				for(int c : node[current].connected) {
+					weight[c]-=1;
+				}
+			}
+			
+		}
+	}
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T= 10;
+		
+		for(int tc=1; tc<T+1; tc++) {
+			st= new StringTokenizer(br.readLine());
+			V= Integer.parseInt(st.nextToken());
+			E= Integer.parseInt(st.nextToken());
+			
+			node= new Node[V+1]; // 0번 index 사용x
+			weight= new int[V+1];
+			
+			for(int i=1; i<V+1; i++) {
+				node[i]= new Node();
+			}
+			
+			st= new StringTokenizer(br.readLine());
+			for(int i=0; i<E; i++) {
+				int from= Integer.parseInt(st.nextToken());
+				int to= Integer.parseInt(st.nextToken());
+				node[from].connected.add(to);
+				weight[to]+=1;
+			}
+			
+			System.out.print("#" + tc + " ");
+			solution();
+			System.out.println();
+		}
 	}
 
 }
