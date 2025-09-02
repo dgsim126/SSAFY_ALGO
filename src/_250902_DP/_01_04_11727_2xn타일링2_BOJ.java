1f698ce32faf3e1b89f0e8eebe4b2bc38cc1295a
dgsim126 <dgsim126@gmail.com>
2025-09-02 17:32:18 +0900

    1932_BOJ ongoing

--- src/_250902_DP/_01_04_11727_2xn타일링2_BOJ.java
+++ src/_250902_DP/_01_04_11727_2xn타일링2_BOJ.java
@@ -26,11 +26,11 @@ public class _01_04_11727_2xn타일링2_BOJ {
 			return 1;
 		}
 		if(n==2) {
-			return 2;
+			return 3;
 		}
 		
 		dp[1]= 1;
-		dp[2]= 2;
+		dp[2]= 3;
 		
 		for(int i=3; i<n+1; i++) {
 			dp[i]= (dp[i-1]+dp[i-2]*2)%10007;
