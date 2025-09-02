f3efd0ec6fd14e8588a8b486da67cc1cbf13a39d
dgsim126 <dgsim126@gmail.com>
2025-09-02 09:35:35 +0900

    feat:test

---
+++ src/_250902/_01_1753_최단경로_BOJ.java
@@ -0,0 +1,10 @@
+package _250902;
+
+public class _01_1753_최단경로_BOJ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/_250902/_02_1916_최소비용구하기.java
@@ -0,0 +1,10 @@
+package _250902;
+
+public class _02_1916_최소비용구하기 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
